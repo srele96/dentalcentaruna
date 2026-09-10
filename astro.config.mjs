b77aa890de0a0eeeebc5d5f2b087d05428a9614b
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2026-09-10 22:34:31 +0200

    add code...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  i18n: {
+    locales: ['sr'],
+    defaultLocale: 'sr',
+    routing: { prefixDefaultLocale: false },
+  },
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
